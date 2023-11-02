13806299d1b972963644b4dd9bd3313737d78905
egamboa63 <59739401+egamboa63@users.noreply.github.com>
2023-11-02 12:01:51 -0500

    :sparkles:  parcial segundo corte
    parcial segundo corte

--- demo/src/main/java/com/planticasalquiler/demo/controllers/AdminControllers.java
+++ demo/src/main/java/com/planticasalquiler/demo/controllers/AdminControllers.java
@@ -2,14 +2,16 @@ package com.planticasalquiler.demo.controllers;
 
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
@@ -84,6 +86,43 @@ public String guardar(@Valid Empleado empleado, BindingResult result, Model mode
     status.setComplete();
     return "redirect:empleadosRegistrados";
 }
+
+    @RequestMapping(value = "/eliminarEmpleado/{id}")
+    public String eliminarEmpleado(@PathVariable Long id) {
+        empleadoRepository.deleteById(id); // Elimina el cliente por su ID
+        return "redirect:/empleadosRegistrados"; // Redirige a la página de lista de clientes o a donde desees
+    }
+
+    @GetMapping("/editarEmpleado/{id}")
+    public String mostrarFormularioEdicionEmpleado(@PathVariable Long id, Model model) {
+        model.addAttribute("roles", rolRepository.findAll());
+        Empleado empleado = empleadoRepository.findById(id).orElse(null);
+        if (empleado != null) {
+            model.addAttribute("empleado", empleado);
+            return "formularioEditarEmpleado";
+        } else {
+            // Manejar el caso en el que el cliente no existe
+            return "redirect:/empleadosRegistrados";
+        }
+    }
+
+    @RequestMapping(value = "/editarEmpleado/{id}", method = RequestMethod.POST)
+    public String procesarEdicionEmpleado(@PathVariable Long id, @ModelAttribute Empleado empleado) {
+        Empleado empleadoExistente = empleadoRepository.findById(id).orElse(null);
+
+        if (empleadoExistente != null) {
+            // Actualizar los datos del cliente existente con los datos del formulario
+            empleadoExistente.setNombre(empleado.getNombre());
+            empleadoExistente.setTelefono(empleado.getTelefono());
+            empleadoExistente.setRoles(empleado.getRoles());
+            empleadoExistente.setUsuario(empleado.getUsuario());
+            // Actualiza otros campos según sea necesario
+
+            empleadoRepository.save(empleadoExistente);
+        }
+
+        return "redirect:/empleadosRegistrados";
+    }
 //fin controllers empleado
 
 //controllers clientes
@@ -114,10 +153,41 @@ public String guardar(@Valid Empleado empleado, BindingResult result, Model mode
         return "redirect:nuevaFactura";
     }
 
-    @RequestMapping(value = "/eliminar/{id}")
+    @RequestMapping(value = "/eliminarCliente/{id}")
     public String eliminarCliente(@PathVariable Long id) {
         clienteRepository.deleteById(id); // Elimina el cliente por su ID
         return "redirect:/clientesRegistrados"; // Redirige a la página de lista de clientes o a donde desees
+    }
+
+    @GetMapping("/editarCliente/{id}")
+    public String mostrarFormularioEdicion(@PathVariable Long id, Model model) {
+        Cliente cliente = clienteRepository.findById(id).orElse(null);
+
+        if (cliente != null) {
+            model.addAttribute("cliente", cliente);
+            return "formularioEditarCliente";
+        } else {
+            // Manejar el caso en el que el cliente no existe
+            return "redirect:/clientesRegistrados";
+        }
+    }
+
+    @RequestMapping(value = "/editarCliente/{id}", method = RequestMethod.POST)
+    public String procesarEdicionCliente(@PathVariable Long id, @ModelAttribute Cliente cliente) {
+        Cliente clienteExistente = clienteRepository.findById(id).orElse(null);
+
+        if (clienteExistente != null) {
+            // Actualizar los datos del cliente existente con los datos del formulario
+            clienteExistente.setNombre(cliente.getNombre());
+            clienteExistente.setDireccion(cliente.getDireccion());
+            clienteExistente.setTelefono(cliente.getTelefono());
+            clienteExistente.setDni(cliente.getDni());
+            // Actualiza otros campos según sea necesario
+
+            clienteRepository.save(clienteExistente);
+        }
+
+        return "redirect:/clientesRegistrados";
     }
     //fin controllers clientes
 
@@ -159,6 +229,40 @@ public String guardar(@Valid Empleado empleado, BindingResult result, Model mode
         status.setComplete();
         return "redirect:productosRegistrados";
     }
+
+    // @RequestMapping(value = "/eliminarProducto/{id}")
+    // public String eliminarProducto(@PathVariable Long id) { 
+    //     productoRepository.deleteById(id); // Elimina el cliente por su ID
+    //     return "redirect:/productosRegistrados"; // Redirige a la página de lista de clientes o a donde desees
+    // }
+
+    @GetMapping("/editarProducto/{id}")
+    public String mostrarFormularioEdicionProducto(@PathVariable Long id, Model model) {
+        Producto producto = productoRepository.findById(id).orElse(null);
+        if (producto != null) {
+            model.addAttribute("producto", producto);
+            return "formularioEditarProducto";
+        } else {
+            // Manejar el caso en el que el cliente no existe
+            return "redirect:/productosRegistrados";
+        }
+    }
+
+    @RequestMapping(value = "/editarProducto/{id}", method = RequestMethod.POST)
+    public String procesarEdicionProdcuto(@PathVariable Long id, @ModelAttribute Producto producto) {
+         Producto productoExistente = productoRepository.findById(id).orElse(null);
+
+        if (productoExistente != null) {
+            // Actualizar los datos del cliente existente con los datos del formulario
+            productoExistente.setNombre(producto.getNombre());
+            productoExistente.setPrecioDia(producto.getPrecioDia());
+            // Actualiza otros campos según sea necesario
+
+            productoRepository.save(productoExistente);
+        }
+
+        return "redirect:/productosRegistrados";
+    }
     
     //fin controllers productos
 
@@ -188,7 +292,38 @@ public String guardar(@Valid Empleado empleado, BindingResult result, Model mode
 
         inventarioRepository.save(inventario);
         status.setComplete();
-        return "redirect:productosRegistrados";
+        return "redirect:inventario";
+    }
+
+
+
+    @GetMapping("/editarInventario/{id}")
+    public String mostrarFormularioEdicionInventario(@PathVariable Long id, Model model) {
+         List<Producto> productos = productoRepository.finByProductos(0);
+        Inventario inventario = inventarioRepository.findById(id).orElse(null);
+        model.addAttribute("productos", productos);
+        if (inventario != null) {
+            model.addAttribute("inventario", inventario);
+            return "formularioEditarInventario";
+        } else {
+            // Manejar el caso en el que el cliente no existe
+            return "redirect:/inventario";
+        }
+    }
+
+    @RequestMapping(value = "/editarInventario/{id}", method = RequestMethod.POST)
+    public String procesarEdicionInventario(@PathVariable Long id, @ModelAttribute Inventario inventario) {
+        Inventario inventarioExistente = inventarioRepository.findById(id).orElse(null);
+
+        if (inventarioExistente != null) {
+            // Actualizar los datos del cliente existente con los datos del formulario
+            inventarioExistente.setEstado(inventario.getEstado());
+            // Actualiza otros campos según sea necesario
+
+            inventarioRepository.save(inventarioExistente);
+        }
+
+        return "redirect:/inventario";
     }
 
     // fin controllers inventario
