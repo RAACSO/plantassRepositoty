e3a205c175bcda2dd713778c43aaac745aebaa4c
egamboa63 <59739401+egamboa63@users.noreply.github.com>
2023-11-30 18:06:26 -0500

    :+1:
    ss

--- src/main/java/com/planticasalquiler/demo/models/Erole.java
+++
@@ -1,8 +0,0 @@
-package com.planticasalquiler.demo.models;
-
-public enum Erole {
-    ADMIN,
-    USER
-    
-}
-
--- src/main/java/com/planticasalquiler/demo/service/impl/IEmpleadoService.java
+++
@@ -1,7 +0,0 @@
-package com.planticasalquiler.demo.service.impl;
-
-import com.planticasalquiler.demo.models.Empleado;
-
-public interface IEmpleadoService {
-    public void save(Empleado empleado);
-}
